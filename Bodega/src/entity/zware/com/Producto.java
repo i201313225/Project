eb1abc86d37c65805479859d04854d711c51eafb
i201313225 <i201313225@cibertec.edu.pe>
2016-06-04 19:09:57 -0500

    Se agregó el constructor y los metodos Get y Set a la clase producto

--- Bodega/src/entity/zware/com/Producto.java
+++ Bodega/src/entity/zware/com/Producto.java
@@ -3,6 +3,37 @@ package entity.zware.com;
 public class Producto {
 	private int codigo;
 	private String descripcion;
-	private double precio;
+	private double precioUnitario;
+
+	public Producto(int codigo, String descripcion, double precioUnitario) {
+		super();
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
 
 }
